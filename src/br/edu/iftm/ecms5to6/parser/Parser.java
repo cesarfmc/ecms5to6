8b9d5ce5f97d79fcd28bd178416042182fc06ac5
matheus <matheus@Ubuntu-Z450LA>
2017-11-23 13:36:08 -0200

    Realizar modificações no arquivo de entrada!

--- src/br/edu/iftm/ecms5to6/parser/Parser.java
+++ src/br/edu/iftm/ecms5to6/parser/Parser.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 import javax.json.Json;
 import javax.json.JsonArray;
+import javax.json.JsonNumber;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
@@ -24,6 +25,7 @@ import rinoceronte.Esprima;
 public class Parser {
 
 	private String filePath;
+	private String obj;
 
 
 	public Parser(String filePath) {
@@ -39,13 +41,13 @@ public class Parser {
 			//Gerar arquivo json
 			Esprima.parse(fileJS, fileJSON);
 
-			//Carregar a arquivo JSON para objetos Java
+			//Carregar o arquivo JSON para objetos Java
 			JsonReader jsonReader = Json.createReader(new FileReader(fileJSON));
 			JsonObject jsonObjectMain = jsonReader.readObject();
 
 			
 			System.out.println(jsonObjectMain.toString());
-
+            
 			String typeOut= (String ) jsonObjectMain.getString("type");
 			String typeIn= null;
 			String typeMiddle1= null;
@@ -57,43 +59,52 @@ public class Parser {
 			
 			String name= null,operator=null; int valueL=0,valueR=0;
 			
+			
 			JsonObjectBuilder tree = Json.createObjectBuilder();
 			tree = tree.add("type", "Program");
 			JsonArray body1 = jsonObjectMain.getJsonArray("body");
+			
 			JsonObjectBuilder tree2 = Json.createObjectBuilder();
+			
+			JsonObject jsonObjectMember1= null;
+			
 			for(JsonValue member: body1){
-				JsonObject jsonObjectMember1 = (JsonObject)member;
-				Set<Entry<String,JsonValue>> myset = jsonObjectMember1.entrySet();
+			
+				jsonObjectMember1 = (JsonObject)member;
+				
+				tree2= buildTree (jsonObjectMember1,tree2);
+				
+				//Set<Entry<String,JsonValue>> myset = jsonObjectMember1.entrySet();
+				
+				
+				/*
 				for (Entry<String, JsonValue> entry : myset) {
+					
 					if (entry.getValue() instanceof JsonArray){
+						
 						System.out.println("JsonArray");
+						
+						System.out.println("teste: "+entry.getValue());
+						
 					}else if (entry.getValue() instanceof JsonObject){
 						System.out.println("JsonObject"); 
 					}
 					else if (entry.getValue() instanceof JsonString){
-						System.out.println("JsonString"); 
+						System.out.println("JsonString: "+entry.getValue()); 
 					}else{
 						System.out.println("error");
 					}
 					tree2 = tree2.add(entry.getKey(),entry.getValue());
 				}
+				*/
 			}	
 			
 			tree = tree.add("body", Json.createArrayBuilder().add(tree2));
 			JsonObject jsonObjectNew = tree.build();
 			System.out.println(jsonObjectNew);
-		
-			/*
-			 * Para alterar o valor da esquerda altere a variável "valueL";
-			 * Para alterar o valor da direita altere a variável "valueR";
-			 * Para alterar o operador altere a variável "operator";
-			 * Para alterar o nome da variável altere a variável "name";
-			 * 
-			 * Realizar a alteração dos valores abaixo do comentário!!!
-			 */
-			
-			// Exemplo;
-			
+	
+			 /*
+			 
 			valueL= 100;
 			valueR= 12;
 			name= "Expressão";
@@ -101,7 +112,7 @@ public class Parser {
 			
 			// criando a árvore.
 
-			/*JsonObject tree3 = Json.createObjectBuilder().add("type", typeOut)
+			JsonObject tree3 = Json.createObjectBuilder().add("type", typeOut)
 					.add("body", Json.createArrayBuilder().add(Json.createObjectBuilder()
 							.add("type", typeIn).add("declarations",Json.createArrayBuilder().add(Json.createObjectBuilder()
 									.add("type",typeMiddle1).add("id", Json.createObjectBuilder()
@@ -113,8 +124,10 @@ public class Parser {
 													.add("Value",valueL)
 													.add("raw", valueL)).add("right",Json.createObjectBuilder()
 															.add("type",last).add("value",valueR).add("raw",valueR))))).add("kind",kind))).build();
-			System.out.println("\n\ntree: "+tree);
+			System.out.println("\n\ntree: "+tree3);
+			
 			*/
+			
 			//código para o matheus adicionar
 
 			//Gerar o novo arquivo json
@@ -132,7 +145,64 @@ public class Parser {
 			System.out.println("\nPrograma finalizado.");
 		}
 	}
+  
 
-
+	private JsonObjectBuilder buildTree (JsonObject jsonObject,JsonObjectBuilder tree2) {
+		
+		Set<Entry<String,JsonValue>> myset= jsonObject.entrySet();
+		
+		for (Entry<String, JsonValue> entry : myset) {
+			
+			if (entry.getValue() instanceof JsonArray){
+				
+				JsonObject object= null;
+				
+				JsonArray array= (JsonArray) entry.getValue();
+				
+				for(JsonValue obj : array ) {
+					
+					 object= (JsonObject) obj;
+					 
+					 tree2 = tree2.add(entry.getKey(), object);
+						
+					 buildTree(object,tree2);
+				}
+				
+			}else if (entry.getValue() instanceof JsonObject){
+				
+				JsonObject obj1= (JsonObject) entry.getValue();
+				tree2= tree2.add(entry.getKey(), entry.getValue());
+				
+				buildTree(obj1,tree2);
+			}
+			else if (entry.getValue() instanceof JsonString){
+				
+				if(entry.getValue().equals("answer")) {
+					
+				  tree2 = tree2.add(entry.getKey(),"resposta");
+				}else {
+					
+					tree2 = tree2.add(entry.getKey(),entry.getValue());	
+				}
+				
+			}else if (entry.getValue() instanceof JsonNumber){
+				
+				int i= 6;
+				int j= 7;
+				
+				if(entry.getValue().equals(i)) {
+					 
+					System.out.println("Entrou");
+					tree2 = tree2.add(entry.getKey(),8);
+				}
+				 else if(entry.getValue().equals(j)) {
+					 System.out.println("Entrou no outro");
+					 tree2 = tree2.add(entry.getKey(),9);
+				 }
+			}
+		}
+		return tree2;
+	}
+   
 }
 
