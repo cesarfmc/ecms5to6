e9874a40ebdd93b09a3d5429f95f489d93f14fcc
matheus <matheus@Ubuntu-Z450LA>
2017-11-29 13:40:36 -0200

    Converter valores e criar arquivo de saída.
    obs: Se entry.getvalue() for uma string, não está entrando no "if" de comparação e troca de valores. Está passando direto para o else e adicionando da forma que foi passado por parâmentro!

--- src/br/edu/iftm/ecms5to6/parser/Parser.java
+++ src/br/edu/iftm/ecms5to6/parser/Parser.java
@@ -1,15 +1,16 @@
 package br.edu.iftm.ecms5to6.parser;
 
 import java.io.File;
+
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.json.Json;
 import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonNumber;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -17,7 +18,6 @@ import javax.json.JsonReader;
 import javax.json.JsonString;
 import javax.json.JsonValue;
 import javax.json.JsonWriter;
-
 import jdk.jfr.events.FileWriteEvent;
 import rinoceronte.Escodegen;
 import rinoceronte.Esprima;
@@ -25,7 +25,6 @@ import rinoceronte.Esprima;
 public class Parser {
 
 	private String filePath;
-	private String obj;
 
 
 	public Parser(String filePath) {
@@ -45,20 +44,24 @@ public class Parser {
 			JsonReader jsonReader = Json.createReader(new FileReader(fileJSON));
 			JsonObject jsonObjectMain = jsonReader.readObject();
 
-			
 			JsonObjectBuilder tree = Json.createObjectBuilder();
 			tree = tree.add("type", "Program");
 			JsonArray body1 = jsonObjectMain.getJsonArray("body");
-			
+
+			System.out.println("Im:  "+jsonObjectMain);
+
 			JsonObjectBuilder tree2 = Json.createObjectBuilder();
 			JsonObject jsonObjectMember1= null;
+			JsonArrayBuilder tree3= Json.createArrayBuilder();
+
 			for(JsonValue member: body1){
-				jsonObjectMember1 = (JsonObject)member;
-				tree2 = buildTree (jsonObjectMember1,tree2);
+				jsonObjectMember1 = (JsonObject)member;               
+				tree3= tree3.add(buildTree (jsonObjectMember1,tree2));
+				tree2= Json.createObjectBuilder();
 			}	
-			tree = tree.add("body", Json.createArrayBuilder().add(tree2));
+			tree = tree.add("body", tree3);
 			JsonObject jsonObjectNew = tree.build();
-			System.out.println(jsonObjectNew);
+			System.out.println("Out: "+jsonObjectNew);
 
 			//Gerar o novo arquivo json
 			File fileNewJSON = new File(fileJS.getPath().substring(0, fileJS.getPath().length() - 3) + "_6.json");
@@ -75,28 +78,33 @@ public class Parser {
 			System.out.println("\nPrograma finalizado.");
 		}
 	}
-  
 
 	private JsonObjectBuilder buildTree (JsonObject jsonObject,JsonObjectBuilder tree2) {
-		
+
 		Set<Entry<String,JsonValue>> myset= jsonObject.entrySet();
 		for (Entry<String, JsonValue> entry : myset) {
 			if (entry.getValue() instanceof JsonArray){
 				JsonObject object= null;
+				JsonArrayBuilder aryAux= Json.createArrayBuilder();
+				JsonObjectBuilder treeAux= Json.createObjectBuilder();
 				JsonArray array= (JsonArray) entry.getValue();
 				for(JsonValue obj : array ) {
-					 object= (JsonObject) obj;
-					 tree2 = tree2.add(entry.getKey(), object);
-					 buildTree(object,tree2);
+					object= (JsonObject) obj;
+					aryAux= aryAux.add(buildTree(object,treeAux));
 				}
+				tree2 = tree2.add(entry.getKey(),aryAux);
 			}else if (entry.getValue() instanceof JsonObject){
 				JsonObject obj1= (JsonObject) entry.getValue();
-				tree2= tree2.add(entry.getKey(), entry.getValue());
-				buildTree(obj1,tree2);
+				JsonObjectBuilder treeAux1= Json.createObjectBuilder();
+				tree2= tree2.add(entry.getKey(),buildTree(obj1,treeAux1));
 			}
 			else if (entry.getValue() instanceof JsonString){
-				if(entry.getValue().equals("answer")) {
-				  tree2 = tree2.add(entry.getKey(),"resposta");
+				if(entry.getValue().toString().equals("answer")) {
+					tree2 = tree2.add(entry.getKey(),"resposta");
+				}else if(entry.getValue().toString().equals("6")) {
+					tree2 = tree2.add(entry.getKey(),"8");
+				}else if(entry.getValue().toString().equals("7")) {
+					tree2 = tree2.add(entry.getKey(),"9");
 				}else {
 					tree2 = tree2.add(entry.getKey(),entry.getValue());	
 				}
@@ -104,12 +112,12 @@ public class Parser {
 				if(entry.getValue().toString().equals("6")) {
 					tree2 = tree2.add(entry.getKey(),8);
 				}else if(entry.getValue().toString().equals("7")) {
-					 tree2 = tree2.add(entry.getKey(),9);
+					tree2 = tree2.add(entry.getKey(),9);
+				}else {
+					tree2 = tree2.add(entry.getKey(),entry.getValue());
 				}
 			}
 		}
 		return tree2;
 	}
-   
 }
-
