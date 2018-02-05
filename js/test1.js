50aab5367e8a7371b6f3d6491736ac07063c7e47
Cesar Couto <cesarfmc@gmail.com>
2018-02-05 18:39:42 -0200

    Voltar ao código anterior

--- js/test1.js
+++ js/test1.js
@@ -3,17 +3,4 @@ function Foo(attr){
 	function Action(){
 	}
 }
-var myFoo = new Foo ("foo");
-
-//Classe Foo com o uso da nova palavra-chave
-class Foo {
-	//Construtor da Classe
-	constructor(attr) {
-		//Atributos
-		this.attr = attr;
-		this.Action(); 
-	}
-	//Metodos
-	Action(){} 
-}
 //var myFoo = new Foo ("foo");
